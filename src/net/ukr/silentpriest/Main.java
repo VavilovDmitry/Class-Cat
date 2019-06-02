5f7e47f9c1de9faae456085d769ca9a715a8fef5
VavilovDmitry <51266930+VavilovDmitry@users.noreply.github.com>
2019-06-02 19:34:27 +0300

    Cat
    Описать класс «Cat» (в качестве образца можно взять домашнего питомца).
    Наделить его свойствами и методами. Создать несколько экземпляров объектов
    этого класса. Использовать эти объекты.

---
+++ src/net/ukr/silentpriest/Main.java
@@ -0,0 +1,26 @@
+package net.ukr.silentpriest;
+
+public class Main {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat cat1 = new Cat("Vaska", "Pers", 3);
+		
+		Cat cat2 = new Cat("Petya", "Dvorovoy", 10);
+		
+		
+		System.out.println(cat1);
+		cat1.meow();
+		System.out.println(cat2);
+		cat2.meow();
+		
+		
+
+	}
+
+	
+	}
+
+
